2b9b8dd1738b64602e62748823d79f928839fa6d
Boonanak <sean.bubernak@gmail.com>
2025-02-14 19:01:56 -0800

    Wrote some more of 2-1

--- 2/2-1/main.c
+++ 2/2-1/main.c
@@ -39,6 +39,21 @@ int main()
     printf("Max of plain/signed long long: %d\n", LLONG_MAX);
 
     printf("Min of unsigned long long: %d\n", 0);
-    printf("Max of unsigned long long: %d\n", ULLONG_MAX); 
+    printf("Max of unsigned long long: %d\n", ULLONG_MAX);
+
+    //9 digits of precision
+    printf("Min normalized positive value of float: %d\n", FLT_MIN);
+    printf("Min denormalized positive value of float: %d\n", FLT_TRUE_MIN);
+    printf("Max value of float: %d\n", FLT_MAX);
+
+    //17 digits of precision
+    printf("Min normalized positive value of double: %d\n", DBL_MIN);
+    printf("Min denormalized positive value of double: %d\n", DBL_TRUE_MIN);
+    printf("Max value of double: %d\n", DBL_MAX);
+
+    //Same number of digits of precision as a double on Windows (MSVC)
+    printf("Min normalized positive value of long double: %d\n", LDBL_MIN);
+    printf("Min denormalized positive value of long double: %d\n", LDBL_TRUE_MIN);
+    printf("Max value of long double: %d\n", LDBL_MAX);
 
 }
