c37f242cdda54b20d7bbf539f2ad1ffe6eaf9086
Boonanak <sean.bubernak@gmail.com>
2025-02-18 00:12:59 -0800

    Finished 2-3 and made slight changes to Ch2.txt file

--- 2/2-3/main.c
+++ 2/2-3/main.c
@@ -3,20 +3,26 @@
 
 /*
 Init array of hexbytes
-Get size of hexbytes array
+Init array of the integer representation of the hexbytes
 
-iterate through all hex bytes as long as i is less than the size of all hex bytes
-  print that hex byte
+call htoi function to return int representation of hexbyte array
 
+print int represention (show the parts of this)
 */
 int main()
 {
-  uint8_t hexbytes[] = {0x83, 0x65, 0x61, 0x6E};
-  size_t numhexbytes = sizeof(hexbytes) / sizeof(hexbytes[0]);
-  uint32_t eqIntVal = 0;
+  uint8_t hexbytes[] = {0x83, 0x65, 0x61, 0x6E, 0xFF};
+  size_t size = sizeof(hexbytes) / sizeof(hexbytes[0]);
 
-  printf("Hexstring: ");
-  for (int i = 0; i < numhexbytes; i++) {
-    printf("%02X", hexbytes[i]);
+  printf("Hexbytes Representaton\n");
+  for (int i = 0; i < size; i++) {
+    printf("%02X ", hexbytes[i]);
   }
+
+  printf("\nInteger Representation\n");
+  for (int i = 0; i < size; i++) {
+    printf("%d ", hexbytes[i]);
+  }
+
+  return 0;
 }
