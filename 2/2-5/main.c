d7f6696cf1d42ea169145a3769038557e2848f64
Sean Bubernak <sean.bubernak@gmail.com>
2025-02-26 06:00:49 +0000

    Wrote 2-5 in full

---
+++ 2/2-5/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/*
+Returns: int: index of first location of ocurrance
+Parameters: char *s1: pointer to first element of s1
+            char *s2: pointer to first element of s2
+            int l1: length of s1
+            int l2: length of s2
+  go through each 
+*/
+int anyh(char *s1, char *s2, int l1, int l2) 
+{
+
+  for (int i = 0; i < l1; i++) {
+    for (int j = 0; j < l2; j++) {
+      if (s1[i] == s2[j]) {
+        return i;
+      }
+    } 
+  }
+
+  return -1;
+}
+
+/*
+return 0 on successful run, no param
+  declare string s1 and populate
+  declare string s2 and populate
+  get and store length of s1 array, l1
+  get and store length of s2 array, l2
+  call anyh on s1, s2, l1, l2
+  if the index returned is not -1
+    print first index of ocurrence
+  else
+    print that no matches were found
+*/
+int main()
+{
+  char s1[] = "Applebottom";
+  char s2[] = "bot";
+  int l1 = sizeof(s1) - 1;
+  int l2 = sizeof(s2) - 1;
+
+  int index = anyh(s1, s2, l1, l2);
+
+  if (index != -1) {
+    printf("The first occurence of s2 in s1 is %d. \n", index);
+  } else {
+    printf("No match found of any of the characters in s1 in s2");
+  }
+
+  return 0;
+}
